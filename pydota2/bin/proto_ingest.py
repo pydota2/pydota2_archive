00db8209e75dac709ebc0f5737bbd8cd89f0de5f
Nostrademous <nostrademous@hotmail.com>
2017-11-01 09:16:52 -0400

    abstracting connecting to localhost for obtaining protobufs so it can easily be reused

--- pydota2/bin/proto_ingest.py
+++ pydota2/bin/proto_ingest.py
@@ -22,89 +22,27 @@
 #       configured.
 # Example: -botworldstatetosocket_radiant 12120 -botworldstatetosocket_dire 12121
 #          -botworldstatetosocket_frames 10 -botworldstatesocket_threaded
-"""Acquire CMsgBotWorldState serialized data."""
+
+"""Save protobuf serialized data from gameplay to replays directory."""
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import os
-import socket
-import threading
-import time
-
-from struct import *
-from datetime import datetime
-
-import six
-from six.moves import queue
+from pydota2.lib.proto_connector import ProtoThread
 
 from absl import app
 
-HOST = '127.0.0.1'      # The remote host
-RADIANT_PORT    = 12120 # The same port as used by the server
-DIRE_PORT       = 12121 # The same port as used by the server
-DIR_REPLAY      = 'replays'
-
-threadLock = threading.Lock()
-sDate = f"{datetime.now():%Y_%m_%d_%H%M_}"
-
-def saveProtoToFile(fname, binData):
-    with open(fname, 'bw') as f:
-        f.write(binData)
-
-class protoThread (threading.Thread):
-    def __init__(self, threadID, name, port):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.port = port
-      
-    def run(self):
-        print("Starting Thread %d for %s" % (self.threadID, self.name))
-        dir_path = os.path.join(DIR_REPLAY, sDate + self.name)
-        # check if directory exists, if not, make it
-        if not os.path.exists(dir_path):
-            os.makedirs(dir_path)
-        self.processProto(dir_path)
-      
-    def processProto(self, dir_path):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((HOST, self.port))
-
-            i = 0
-            while i < 1000000:
-                binSize = s.recv(4)
-
-                if not binSize:
-                    print("%s proto stream not found! Exiting!" % (self.name))
-                    break
-
-                if binSize:
-                    protoSize = unpack("<I", binSize)
-                    print("%s protoSize: %d" % (self.name, protoSize[0]))
-                    binData = s.recv(protoSize[0])
-
-                    # Get lock to synchronize threads
-                    threadLock.acquire()
-                    saveProtoToFile(os.path.join(dir_path, str(i).zfill(6) + '.bin'), binData)
-                    # Free lock to release for next thread
-                    threadLock.release()
-                    i += 1
-
-            print("Closing Socket")
-            s.close()
 
 def main(unused_argv):
     threads = []
     try:
-        # Create our threads
-        t1 = protoThread(1, 'Radiant', RADIANT_PORT)
-        t2 = protoThread(2, 'Dire', DIRE_PORT)
+        # Create our threads and set func callback to our save to file function
+        t1 = ProtoThread(1, 'Radiant', save_proto=True)
+        t2 = ProtoThread(2, 'Dire', save_proto=True)
 
         # Start our threads
         t1.start()
-        time.sleep(1)
         t2.start()
 
         # Append to list
@@ -116,6 +54,7 @@ def main(unused_argv):
         # Wait for all threads to complete
         for t in threads:
             t.join()
+
 
 if __name__ == "__main__":
     app.run(main)
---
+++ pydota2/lib/proto_connector.py
@@ -0,0 +1,105 @@
+# NOTE: This code is to a large degree based on DeepMind work for
+#       AI in StarCraft2, just ported towards the Dota 2 game.
+#       DeepMind's License is posted below.
+
+#!/usr/bin/python
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# NOTE: This file assumes that your Dota2 launch options are properly
+#       configured.
+# Example: -botworldstatetosocket_radiant 12120 -botworldstatetosocket_dire 12121
+#          -botworldstatetosocket_frames 10 -botworldstatesocket_threaded
+"""Acquire CMsgBotWorldState serialized data."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import socket
+import threading
+import os
+from datetime import datetime
+
+from struct import *
+
+HOST            = '127.0.0.1'      # The remote host
+RADIANT_PORT    = 12120 # The same port as used by the server
+DIRE_PORT       = 12121 # The same port as used by the server
+DIR_REPLAY      = 'replays'
+
+sDate = "{:%Y_%m_%d_%H%M_}".format(datetime.now())
+
+threadLock = threading.Lock()
+
+
+class ProtoThread(threading.Thread):
+    def __init__(self, threadID, name, save_proto=True, func_callback=None):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name  # should be either 'Radiant' or 'Dire'
+        if self.name == 'Radiant':
+            self.port = RADIANT_PORT
+        else:
+            self.port = DIRE_PORT
+        self.num_proto = 0
+        self.save_proto = save_proto
+        self.func = func_callback
+
+    def run(self):
+        print("Starting Thread %d for %s" % (self.threadID, self.name))
+        self.create_save_directory(os.path.join(DIR_REPLAY, sDate + self.name))
+        self.connect_with_server()
+
+    @staticmethod
+    def save_proto_to_file(self, bin_data):
+        with open(os.path.join(DIR_REPLAY, sDate + self.name, str(self.num_proto).zfill(6) + '.bin'), 'bw') as f:
+            f.write(bin_data)
+
+    @staticmethod
+    def create_save_directory(self, dir_path):
+        # check if directory exists, if not, make it
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+
+    def connect_with_server(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, self.port))
+
+            while True:
+                binSize = s.recv(4)
+
+                if not binSize:
+                    print("%s proto stream not found! Exiting!" % self.name)
+                    break
+
+                if binSize:
+                    protoSize = unpack("<I", binSize)
+                    print("%s protoSize: %d" % (self.name, protoSize[0]))
+                    binData = s.recv(protoSize[0])
+
+                    # Get lock to synchronize threads
+                    threadLock.acquire()
+                    if self.save_proto:
+                        self.save_proto_to_file(binData)
+                    if self.func:
+                        self.func(binData)
+                    # Free lock to release for next thread
+                    threadLock.release()
+
+                    self.num_proto += 1
+
+            print("Closing Socket")
+            s.close()
